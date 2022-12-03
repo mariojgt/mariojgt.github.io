bafc6d811fa302ade38cba677862e0c097bf2782
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2022-12-03 15:03:15 +0000

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
     <layout>
         <!-- <navbar /> -->
-
         <!-- Block start here -->
         <block01 />
-
-        <div class="grid p-6">
-            <div>
-                <h1 class="text-4xl font-bold">My Projects</h1>
+        <div
+            class="antialiased w-full bg-gradient-to-br from-gray-800 to-gray-900 text-gray-400 font-inter p-10">
+            <div class="container px-4 mx-auto">
+                <div>
+                    <div id="title" class="text-center my-10">
+                        <h1 class="font-bold text-9xl text-white">{{ emoji }}</h1>
+                    </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly gap-10 pt-10">
+                        <genericBlock title="Wind-Notify"
+                            description="Wind-Notify provide a simple and easy way to display tailwind and daisy UI toast notification with almost zero configuration"
+                            link="https://github.com/mariojgt/wind-notify"
+                            img="https://raw.githubusercontent.com/mariojgt/wind-notify/main/art/logo_large.png"
+                            tags="tailwind, javascript" />
+                        <genericBlock title="Skeleton Admin"
+                            description="Laravel starting point with frontend and backend autentication, comes with 2fa, vue js, tailwind out of the box."
+                            link="https://github.com/mariojgt/skeleton-admin"
+                            img="https://raw.githubusercontent.com/mariojgt/skeleton-admin/master/Publish/Art/logo.png"
+                            tags="laravel,tailwind, vuejs" />
+                        <genericBlock title="Skeleton Admin"
+                            description="Laravel starting point with frontend and backend autentication, comes with 2fa, vue js, tailwind out of the box."
+                            link="https://github.com/mariojgt/skeleton-admin"
+                            img="https://raw.githubusercontent.com/mariojgt/skeleton-admin/master/Publish/Art/logo.png"
+                            tags="laravel,tailwind, vuejs" />
+                    </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly gap-10 pt-10">
+                        <genericBlock title="Castle"
+                            description="This Laravel packages has been design to quickly add 2 steps verifications in am very simple way and very easy to expand."
+                            link="https://github.com/mariojgt/castle"
+                            img="https://raw.githubusercontent.com/mariojgt/castle/main/Publish/Art/logo.png"
+                            tags="laravel,vuejs,tailwind,2fa" />
+                        <genericBlock title="Biscotto"
+                            description="Laravel cookie policy packages to help your website with the GDPR rules."
+                            link="https://github.com/mariojgt/biscotto"
+                            img="https://raw.githubusercontent.com/mariojgt/biscotto/main/Publish/Image/Biscotto.png"
+                            tags="laravel,vanilia js" />
+                        <genericBlock title="Gateway"
+                            description="This is a Laravel package design to make checkout easy and clean to implement, avaliable integration (Stripe), in the future we going to have some more integrations with others gateways, the paypal is almost there"
+                            link="https://github.com/mariojgt/gateway"
+                            img="https://raw.githubusercontent.com/mariojgt/gateway/main/Publish/Art/logo.png"
+                            tags="laravel,php, stripe, paypal" />
+                    </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly gap-10 pt-10">
+                        <genericBlock title="Magnifier" description="A laravel media manager easy to use an integrate."
+                            link="https://github.com/mariojgt/magnifier"
+                            img="https://raw.githubusercontent.com/mariojgt/magnifier/main/Publish/art/logo.png"
+                            tags="laravel,api, tailwind" />
+                        <genericBlock title="UnrealRpgMapMaker"
+                            description="Comple solution to make a RPG map maker, with a lot of features, like a radar, waypoints and more."
+                            link="https://github.com/mariojgt/UnrealRpgMapMaker"
+                            img="https://raw.githubusercontent.com/mariojgt/UnrealRpgMapMaker/main/Art/logo.png"
+                            tags="unreal engine 5" />
+                        <genericBlock title="RpgClimbing"
+                            description="Zelda style climbing system for unreal engine, this project was desing to work standalone and almos 0 configurations"
+                            link="https://github.com/mariojgt/RpgClimbing"
+                            img="https://raw.githubusercontent.com/mariojgt/RpgClimbing/main/Art/logo.png"
+                            tags="unreal engine 5" />
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div class="
+        <!-- <div class="
         flex flex-col
         lg:grid lg:gap-2
         2xl:gap-6
@@ -22,46 +74,17 @@
         card
         bg-base-300
       ">
+
             <div>
-                <genericBlock title="Wind-Notify"
-                    description="Wind-Notify provide a simple and easy way to display tailwind and daisy UI toast notification with almost zero configuration"
-                    link="https://github.com/mariojgt/wind-notify"
-                    img="https://raw.githubusercontent.com/mariojgt/wind-notify/main/art/logo_large.png"
-                    tags="tailwind, javascript" />
+
             </div>
             <div>
-                <genericBlock title="Skeleton Admin"
-                    description="Laravel starting point with frontend and backend autentication, comes with 2fa, vue js, tailwind out of the box."
-                    link="https://github.com/mariojgt/skeleton-admin"
-                    img="https://raw.githubusercontent.com/mariojgt/skeleton-admin/master/Publish/Art/logo.png"
-                    tags="laravel,tailwind, vuejs" />
+
             </div>
             <div>
-                <genericBlock title="Castle"
-                    description="This Laravel packages has been design to quickly add 2 steps verifications in am very simple way and very easy to expand."
-                    link="https://github.com/mariojgt/castle"
-                    img="https://raw.githubusercontent.com/mariojgt/castle/main/Publish/Art/logo.png"
-                    tags="laravel,vuejs,tailwind,2fa" />
             </div>
             <div>
-                <genericBlock title="Biscotto"
-                    description="Laravel cookie policy packages to help your website with the GDPR rules."
-                    link="https://github.com/mariojgt/biscotto"
-                    img="https://raw.githubusercontent.com/mariojgt/biscotto/main/Publish/Image/Biscotto.png"
-                    tags="laravel,vanilia js" />
-            </div>
-            <div>
-                <genericBlock title="Gateway"
-                    description="This is a Laravel package design to make checkout easy and clean to implement, avaliable integration (Stripe), in the future we going to have some more integrations with others gateways, the paypal is almost there"
-                    link="https://github.com/mariojgt/gateway"
-                    img="https://raw.githubusercontent.com/mariojgt/gateway/main/Publish/Art/logo.png"
-                    tags="laravel,php, stripe, paypal" />
-            </div>
-            <div>
-                <genericBlock title="Magnifier" description="A laravel media manager easy to use an integrate."
-                    link="https://github.com/mariojgt/magnifier"
-                    img="https://raw.githubusercontent.com/mariojgt/magnifier/main/Publish/art/logo.png"
-                    tags="laravel,api, tailwind" />
+
             </div>
             <div>
                 <genericBlock title="UnrealRpgMapMaker"
@@ -77,7 +100,7 @@
                     img="https://raw.githubusercontent.com/mariojgt/RpgClimbing/main/Art/logo.png"
                     tags="unreal engine 5" />
             </div>
-        </div>
+        </div> -->
     </layout>
 </template>
 
@@ -93,6 +116,16 @@ import genericBlock from "./components/global/GenericBlock.vue";
 import { randomTheme } from "./helpers/core";
 // Update the page tittle
 document.title = "Mario Tarosso | Projects";
+
+let emoji = $ref('😎');
+const randomEmoji = async (id, name) => {
+    const emojisList = ['😎', '😊', '👨‍💻', '😁', '🙄', '🥸'];
+    emoji = emojisList[Math.floor(Math.random() * emojisList.length)];
+};
+
+setInterval(() => {
+    randomEmoji();
+}, 3000);
 
 randomTheme();
 </script>
--- src/components/global/GenericBlock.vue
+++ src/components/global/GenericBlock.vue
@@ -1,58 +1,51 @@
 <template>
-  <div class="card w-96 bg-base-300 shadow-xl">
-    <figure>
-      <img :src="props.img" alt="Shoes" />
-    </figure>
-    <div class="card-body">
-      <h2 class="card-title">
-        {{ props.title }}
-        <div
-          v-for="(item, index) in tagsLoop"
-          :key="index"
-          class="badge badge-primary"
-        >
-          {{ item }}
+    <div class="card w-full border-2 border-primary shadow-xl transform hover:shadow-2xl hover:scale-105 transition duration-200 ease-in">
+        <figure class="px-5 pt-5">
+            <img :src="props.img" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">{{ props.title }}</h2>
+            <div v-for="(item, index) in tagsLoop" :key="index" class="badge badge-primary">
+                    {{ item }}
+            </div>
+            <p>{{ props.description }}</p>
+            <div class="card-actions">
+                <a class="btn btn-primary" :href="props.link">Read More</a>
+            </div>
         </div>
-      </h2>
-      <p>{{ props.description }}</p>
-      <div class="card-actions justify-end">
-        <a class="btn btn-primary" :href="props.link">Read More</a>
-      </div>
-
     </div>
-  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-  title: {
-    type: String,
-    default: "mariojgt is heredude",
-  },
-  description: {
-    type: String,
-    default: "infor here",
-  },
-  link: {
-    type: String,
-    default: "https://tailwindcss.com/docs/grid-template-columns",
-  },
-  img: {
-    type: String,
-    default:
-      "https://raw.githubusercontent.com/mariojgt/skeleton/master/Publish/Art/skeleton.png",
-  },
-  tags: {
-    type: String,
-    default: "php,laravel",
-  },
+    title: {
+        type: String,
+        default: "mariojgt is heredude",
+    },
+    description: {
+        type: String,
+        default: "infor here",
+    },
+    link: {
+        type: String,
+        default: "https://tailwindcss.com/docs/grid-template-columns",
+    },
+    img: {
+        type: String,
+        default:
+            "https://raw.githubusercontent.com/mariojgt/skeleton/master/Publish/Art/skeleton.png",
+    },
+    tags: {
+        type: String,
+        default: "php,laravel",
+    },
 });
 
 let tagsLoop = $ref();
 
 const projectTags = async () => {
-  // Build the tags spereate by comma
-  tagsLoop = props.tags.split(",");
+    // Build the tags spereate by comma
+    tagsLoop = props.tags.split(",");
 };
 
 projectTags();
