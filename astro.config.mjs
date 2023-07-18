010ee3fb17bc57a3e48cbbc60835e8c23a1440ce
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2023-07-18 19:31:56 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mariojgt.github.io',
-    // base: '/mariojgt.github.io',
+  site: 'https://mariojgt.github.io'
+  // base: '/mariojgt.github.io',
+  ,
+  integrations: [partytown()]
 });
